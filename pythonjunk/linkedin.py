78b55e40bbee1f5370b14b4edfe3d7162c4f7d98
lacunadream <d.s.yong@lse.ac.uk>
2016-03-12 12:33:39 +0000

    sync

--- pythonjunk/linkedin.py
+++ pythonjunk/linkedin.py
@@ -16,4 +16,14 @@ randUA = random.randint(0,657)
 def randomUA():
 	x = random.randint(0,657)
 	return x
+kk = ua[randUA]
+print(ua[randUA])
+headersX = {
+	'User-Agent' : 'Mozilla/3.04 (compatible; NCBrowser/2.35; ANTFresco/2.17; RISC OS-NC 5.13 Laz1UK1309)'
+}
 	
+r = requests.get(rootURL, headers=headersX)
+source = bs4.BeautifulSoup(r.content, "lxml")
+# print(r.text)
+k = source.find("div", {"class":"browse-map"})
+print(k)
